75d358a69d7dc9dce784540804f45e1be4a70bd6
Jakub Máca <xmaca@fi.muni.cz>
2022-11-11 20:29:16 +0100

    Deleted

--- src/judy.h
+++
@@ -1,9 +0,0 @@
-#include <Judy.h>
-
-#define _GNU_SOURCE
-#define JUDYERROR_SAMPLE 1 // use default Judy error handler
-#define MAXLINELEN 1000000 // define maximum string length
-
-void insert(Pvoid_t *ruby_array, char *key, char *value);
-
-Word_t find(Pvoid_t *ruby_array, char *key);
